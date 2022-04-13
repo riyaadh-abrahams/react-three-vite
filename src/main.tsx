df27d6cb41216679a7341b1e570590f6f396b711
Riyaadh <riyaadh.abr@gmail.com>
2022-04-13 20:35:51 +0200

    Update Versions

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 
-ReactDOM.render(
+const container = document.getElementById("root")!;
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
